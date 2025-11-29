7e4a89fb3bc7ebc266a2422f868a0c1862151ee2
Petar Cevriz <pcevriz95@gmail.com>
2025-11-29 18:09:21 +0100

    Add initial lambda function for listing tasks

---
+++ backend/tests/tasks/test_list.py
@@ -0,0 +1,19 @@
+from src.tasks.list import list as list_tasks
+
+
+def test_list_tasks_on_success_returns_200():
+    event = {}
+    context = {}
+
+    response = list_tasks(event, context)
+
+    assert response["statusCode"] == 200
+
+def test_list_tasks_on_success_returns_apropriate_headers():
+    event = {}
+    context = {}
+
+    response = list_tasks(event, context)
+
+    assert response["headers"]["Content-Type"] == "application/json"
+    assert response["headers"]["Access-Control-Allow-Origin"] == "*"
