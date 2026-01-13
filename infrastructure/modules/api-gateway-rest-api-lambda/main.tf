5ea269469df32bcf27f33fdb88997321ef6604ff
Petar Cevriz <pcevriz95@gmail.com>
2026-01-13 11:27:44 +0100

    Make API layer responsible for the response

--- infrastructure/modules/api-gateway-rest-api-lambda/main.tf
+++ infrastructure/modules/api-gateway-rest-api-lambda/main.tf
@@ -26,8 +26,24 @@ resource "aws_api_gateway_integration" "integration" {
   resource_id             = aws_api_gateway_resource.resource.id
   http_method             = aws_api_gateway_method.method.http_method
   integration_http_method = "POST"
-  type                    = "AWS_PROXY"
+  type                    = "AWS"
   uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "integration_response" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method.http_method
+  status_code = aws_api_gateway_method_response.response_200.status_code
+
+  depends_on = [aws_api_gateway_integration.integration]
 }
 
 # Lambda
