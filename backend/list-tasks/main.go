ccbe37d6f8cad24d792b72bc6354160ae11a1fd9
Petar Cevriz <pcevriz95@gmail.com>
2026-01-15 11:55:02 +0100

    Add a better structure to the go response

--- backend/list-tasks/main.go
+++ backend/list-tasks/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
-	"github.com/aws/aws-lambda-go/lambda"
+    "context"
+    "github.com/aws/aws-lambda-go/lambda"
 )
 
-func hello() (string, error) {
-	return "Hello λ!", nil
+type Response struct {
+    StatusCode int               `json:"statusCode"`
+    Body       string            `json:"body"`
+}
+
+func handler(ctx context.Context) (Response, error) {
+    return Response{
+        StatusCode: 200,
+        Body:       `{"message": "Hello from Go!"}`,
+    }, nil
 }
 
 func main() {
-	lambda.Start(hello)
+    lambda.Start(handler)
 }
