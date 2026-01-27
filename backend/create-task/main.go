92830da10d3d948a7847cd3649cd18f87640291f
Petar Cevriz <pcevriz95@gmail.com>
2026-01-27 14:09:09 +0100

    Create a plan top update create task and make it functional

--- backend/create-task/main.go
+++ backend/create-task/main.go
@@ -10,6 +10,11 @@ type Response struct {
 }
 
 func handler(ctx context.Context) (Response, error) {
+	// Pull data from the request body
+	// At this point we did check if request body is valid via API Gateway
+	// If there is a need, do a domain logic check
+	// Add the data to the DB
+	// Return a response
 	return Response{
 		Body: `{"message": "Create API, hello from Go!"}`,
 	}, nil
