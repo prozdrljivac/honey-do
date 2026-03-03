270e8a28b10d32e3e56c9fd5e4dd1304ccc2c8bd
Petar Cevriz <pcevriz95@gmail.com>
2026-03-03 10:53:00 +0100

    Fix an issue where create task did not work

--- backend/create-task/main.go
+++ backend/create-task/main.go
@@ -71,7 +71,7 @@ func validateRequest(req CreateTaskRequest) error {
 
 func createTask(ctx context.Context, userID, taskID string, req CreateTaskRequest) error {
 	_, err := dynamoClient.PutItem(ctx, &dynamodb.PutItemInput{
-		TableName: aws.String("honey-do-dev-table"), // I need to figure out how this is dynamically pulled
+		TableName: aws.String("honey-do-dev"), // I need to figure out how this is dynamically pulled
 		Item: map[string]types.AttributeValue{
 			"PK":        &types.AttributeValueMemberS{Value: "USER#" + userID},
 			"SK":        &types.AttributeValueMemberS{Value: "TASK#" + taskID},
