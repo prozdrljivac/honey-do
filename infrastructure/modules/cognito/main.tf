4b3ff939b5aa4c23e12fe933f5d2107b442e8d4f
Petar Cevriz <pcevriz95@gmail.com>
2026-03-05 12:26:03 +0100

    Create an initial cognito module

---
+++ infrastructure/modules/cognito/main.tf
@@ -0,0 +1,36 @@
+resource "aws_cognito_user_pool" "pool" {
+    name = "${var.app_name}-${var.environment}"
+
+    username_attributes      = ["email"]
+    auto_verified_attributes = ["email"]
+
+    password_policy {
+        minimum_length    = 8
+        require_lowercase = true
+        require_numbers   = true
+        require_symbols   = true
+        require_uppercase = true
+    }
+}
+
+resource "aws_cognito_user_pool_client" "client" {
+  name         = "${var.app_name}-${var.environment}-client"
+  user_pool_id = aws_cognito_user_pool.pool.id
+
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH"
+  ]
+
+  generate_secret = false
+
+  access_token_validity  = 1
+  id_token_validity      = 1
+  refresh_token_validity = 7
+
+  token_validity_units {
+    access_token  = "hours"
+    id_token      = "hours"
+    refresh_token = "days"
+  }
+}
