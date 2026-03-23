349d553242678a6840cb4c4870db78d76a921fed
Petar Cevriz <pcevriz95@gmail.com>
2026-03-23 10:38:21 +0100

    Add some initial work for getting the task details for a specific task

--- backend/detail-task/main.go
+++ backend/detail-task/main.go
@@ -5,11 +5,27 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type Response struct {
-	Body string `json:"body"`
+type Task struct {
+	ID        string `json:"taskId"`
+	Name      string `json:"name"`
+	Status    string `json:"status"`
+	CreatedBy string `json:"createdBy"`
+	CreatedAt string `json:"createdAt"`
 }
 
-func handler(ctx context.Context) (Response, error) {
+type DetailTaskRequest struct {
+	UserId string `json:userId`
+	Email string `json:email`
+}
+
+type DetailTaskResponse struct {
+	Task Task `json:"task,omitempty"`
+	Error string `json:"error,omitempty"`
+}
+
+func handler(ctx context.Context, req DetailTaskRequest) (Response, error) {
+	userId := req.userId
+	// From the query pull taskId an get task by it, if no task return 404
 	return Response{
 		Body: `{"messsage": "Hello from task details and Go!"}`,
 	}, nil
